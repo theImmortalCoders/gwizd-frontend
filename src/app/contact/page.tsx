f61c6dcae146a6e031cdd18a2134f7bd539328d8
Wiktor-Mazur <zegarekpl.gaming@gmail.com>
2023-09-30 16:35:31 +0200

    contacts

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -1,9 +1,24 @@
 import styles from "./page.module.css";
 
 export default function Contact() {
+  const emergencyNumbers = [
+    { type: "Policja", number: "997" },
+    { type: "Straż Pożarna", number: "998" },
+    { type: "Pogotowie Ratunkowe", number: "999" },
+    { type: "Pogotowie Gazowe", number: "992" },
+    { type: "Pogotowie Energetyczne", number: "991" },
+  ];
+
   return (
-    <div className={styles.contactpagemain}>
-      <h1>Kontakt</h1>
+    <div className="emergency-numbers">
+      <h1>Numery Alarmowe</h1>
+      <ul>
+        {emergencyNumbers.map((item, index) => (
+          <li key={index}>
+            <strong>{item.type}:</strong> {item.number}
+          </li>
+        ))}
+      </ul>
     </div>
   );
 }
