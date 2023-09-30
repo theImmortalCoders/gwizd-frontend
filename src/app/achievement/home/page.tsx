f5885cbf69ef502c298af68d9ca2c79fe3ca0d51
Wiktor-Mazur <zegarekpl.gaming@gmail.com>
2023-10-01 00:50:57 +0200

    achievement

--- src/app/achievement/home/page.tsx
+++ src/app/achievement/home/page.tsx
@@ -5,29 +5,11 @@ import { useState } from "react";
 import AchievementBox from "../components/box/box";
 
 export default function Home() {
-  const [userInfo, setUserInfo] = useState(null);
-
-  const handleGetUserInfo = () => {
-    getUserInfo(setUserInfo);
-  };
-
-  const getUserInfo = (setUserInfo: any) => {
-    fetch("http://localhost:8080/api/user/me", {
-      credentials: "include",
-    })
-      .then((response) => response.json())
-      .then((data: { username: string; email: string }) => {
-        setUserInfo(data);
-        console.log(data);
-      })
-      .catch((error) => console.log("Authorization failed: " + error.message));
-  };
-
   return (
     <div className={styles.achievementpagemain}>
       <div className={styles.backgroundImage}>
         <div className={styles.container}>
-          <h1>Osiągnięcia</h1>
+          <h1>Osiągnięcia:</h1>
           <AchievementBox />
         </div>
       </div>
