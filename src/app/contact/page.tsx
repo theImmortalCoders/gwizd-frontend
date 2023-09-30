27789eff63b0fddd3ceed063656d21616f772573
Wiktor-Mazur <zegarekpl.gaming@gmail.com>
2023-09-30 17:39:50 +0200

    numbers

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -2,23 +2,25 @@ import styles from "./page.module.css";
 
 export default function Contact() {
   const emergencyNumbers = [
+    { type: "Numer alarmowy", number: "112" },
     { type: "Policja", number: "997" },
     { type: "Straż Pożarna", number: "998" },
-    { type: "Pogotowie Ratunkowe", number: "999" },
-    { type: "Pogotowie Gazowe", number: "992" },
-    { type: "Pogotowie Energetyczne", number: "991" },
   ];
 
   return (
-    <div className="emergency-numbers">
-      <h1>Numery Alarmowe</h1>
-      <ul>
-        {emergencyNumbers.map((item, index) => (
-          <li key={index}>
-            <strong>{item.type}:</strong> {item.number}
-          </li>
-        ))}
-      </ul>
+    <div className={styles.backgroundImage}>
+      <div className={styles.contactpagemain}>
+        <h1>Lista numerów alarmowych:</h1>
+        <div className={styles.emergencyNumbers}>
+          <ul>
+            {emergencyNumbers.map((item, index) => (
+              <li key={index}>
+                <strong>- {item.type}:</strong> {item.number}
+              </li>
+            ))}
+          </ul>
+        </div>
+      </div>
     </div>
   );
 }
