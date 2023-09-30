bfd9d2d89be5c947ca517961fb985120bc2b8a1e
Wiktor-Mazur <zegarekpl.gaming@gmail.com>
2023-09-30 20:06:55 +0200

    mapa

--- src/app/maps/page.tsx
+++ src/app/maps/page.tsx
@@ -1,9 +1,27 @@
+"use client";
 import styles from "./page.module.css";
+import { useMemo } from "react";
+import { GoogleMap, useLoadScript, Marker } from "@react-google-maps/api";
 
 export default function Maps() {
+  const { isLoaded, loadError } = useLoadScript({
+    googleMapsApiKey: "AIzaSyATXEtRMP-Yn6KCPDpHTm5CTiYZI5qWqGc",
+  });
+
+  if (loadError) return <div>Error loading maps</div>;
+  if (!isLoaded) return <div>Loading...</div>;
+
+  return <Map />;
+}
+
+function Map() {
   return (
-    <div className={styles.mapspagemain}>
-      <h1>Mapy</h1>
-    </div>
+    <GoogleMap
+      zoom={10}
+      center={{ lat: 44, lng: -80 }}
+      mapContainerClassName="mapContainer"
+    >
+      {/* Dodaj tutaj Marker lub inne elementy mapy, jeśli są potrzebne */}
+    </GoogleMap>
   );
 }
