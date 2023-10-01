a052f7a77a418aa85973385d6be8c5334f87e7ba
marcinbator <marcinbator.ofc@gmail.com>
2023-10-01 10:44:46 +0200

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -17,7 +17,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <html lang="en">
+    <html lang="pl">
       <body className={inter.className}>
         <Navbar />
         {children}
