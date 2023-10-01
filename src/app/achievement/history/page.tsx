e952ae1a730e47118b1840d8947d19a37fd142a0
marcinbator <marcinbator.ofc@gmail.com>
2023-10-01 06:26:59 +0200

    Update page.tsx

--- src/app/achievement/history/page.tsx
+++ src/app/achievement/history/page.tsx
@@ -1,26 +1,40 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import styles from "./page.module.css";
 
 function History() {
-  const historyData = [
-    {
-      id: 1,
-      data: "2023-10-01",
-      typSpotkania: "Spotkanie 1",
-      iloscZwierzat: 3,
-      tytul: "Spotkanie z psami",
-      opis: "Opis spotkania z psami",
-    },
-    {
-      id: 2,
-      data: "2023-10-02",
-      typSpotkania: "Spotkanie 2",
-      iloscZwierzat: 2,
-      tytul: "Spotkanie z kotami",
-      opis: "Opis spotkania z kotami",
-    },
-    // Dodaj więcej danych historii tutaj
-  ];
+  const [user, setUser] = useState([]);
+  const getUser = async () => {
+    const response = await fetch("http://localhost:8080/api/user/me", {
+      credentials: "include",
+    });
+    const user = await response.json();
+    setUser(user.id);
+  };
+  const [reports, setReports] = useState([]);
+  const getReports = async () => {
+    const response = await fetch(
+      `http://localhost:8080/api/report/?userId=${user}`
+    );
+    const reports = await response.json();
+    console.log(reports);
+    setReports(
+      reports.map((rep: any) => {
+        return {
+          id: rep.id,
+          title: rep.title,
+          description: rep.description,
+          type: rep.reportType,
+          animal: rep.animal,
+          quantity: rep.quantity,
+          date: rep.createdDate,
+        };
+      })
+    );
+  };
+  useEffect(() => {
+    getUser();
+    getReports();
+  }, []);
 
   return (
     <div className={styles.historypagemain}>
@@ -31,25 +45,28 @@ function History() {
             <table className={styles.historyTable}>
               <thead>
                 <tr>
-                  <th>ID</th>
-                  <th>Data</th>
-                  <th>Typ spotkania</th>
-                  <th>Ilość</th>
-                  <th>Tytuł</th>
+                  <th>Nazwa</th>
                   <th>Opis</th>
+                  <th>Typ</th>
+                  <th>Zwierzę</th>
+                  <th>Ilość</th>
+                  <th>Data</th>
                 </tr>
               </thead>
               <tbody>
-                {historyData.map((item) => (
-                  <tr key={item.id}>
-                    <td>{item.id}</td>
-                    <td>{item.data}</td>
-                    <td>{item.typSpotkania}</td>
-                    <td>{item.iloscZwierzat}</td>
-                    <td>{item.tytul}</td>
-                    <td>{item.opis}</td>
-                  </tr>
-                ))}
+                {reports.map((rep: any) => {
+                  console.log(rep);
+                  return (
+                    <tr key={rep.id}>
+                      <td>{rep.title}</td>
+                      <td>{rep.description}</td>
+                      <td>{rep.type}</td>
+                      <td>{rep.animal.name}</td>
+                      <td>{rep.quantity}</td>
+                      <td>{rep.date}</td>
+                    </tr>
+                  );
+                })}
               </tbody>
             </table>
           </div>
