fbc5def306ee8f946e28beb0a37fe08e8046ebbc
Wiktor-Mazur <zegarekpl.gaming@gmail.com>
2023-10-01 03:16:33 +0200

    statistic2

--- src/app/statistic/page.tsx
+++ src/app/statistic/page.tsx
@@ -35,54 +35,58 @@ export default function Statistic() {
 
   return (
     <div className={styles.statisticpagemain}>
-      <Typography variant="h3">Statystyki</Typography>
+      <div className={styles.backgroundImage}>
+        <div className={styles.container}>
+          <Typography variant="h3">Statystyki</Typography>
 
-      <div className={styles.filters}>
-        <TextField
-          label="Szukaj"
-          variant="outlined"
-          value={searchTerm}
-          onChange={(e) => setSearchTerm(e.target.value)}
-        />
+          <div className={styles.filters}>
+            <TextField
+              label="Szukaj"
+              variant="outlined"
+              value={searchTerm}
+              onChange={(e) => setSearchTerm(e.target.value)}
+            />
 
-        <FormControl variant="outlined">
-          <InputLabel>Liczba populacji</InputLabel>
-          <Select
-            value={filterPopulation}
-            onChange={(e) => setFilterPopulation(e.target.value)}
-            label="Liczba populacji"
-          >
-            <MenuItem value="">Wszystkie</MenuItem>
-            <MenuItem value="1000">Powyżej 1000</MenuItem>
-            <MenuItem value="5000">Powyżej 5000</MenuItem>
-            <MenuItem value="10000">Powyżej 10000</MenuItem>
-          </Select>
-        </FormControl>
+            <FormControl variant="outlined">
+              <InputLabel>Liczba populacji</InputLabel>
+              <Select
+                value={filterPopulation}
+                onChange={(e) => setFilterPopulation(e.target.value)}
+                label="Liczba populacji"
+              >
+                <MenuItem value="">Wszystkie</MenuItem>
+                <MenuItem value="1000">Powyżej 1000</MenuItem>
+                <MenuItem value="5000">Powyżej 5000</MenuItem>
+                <MenuItem value="10000">Powyżej 10000</MenuItem>
+              </Select>
+            </FormControl>
 
-        <FormControl variant="outlined">
-          <InputLabel>Stopień zagrożenia</InputLabel>
-          <Select
-            value={filterThreatLevel}
-            onChange={(e) => setFilterThreatLevel(e.target.value)}
-            label="Stopień zagrożenia"
-          >
-            <MenuItem value="">Wszystkie</MenuItem>
-            <MenuItem value="Niskie">Niskie</MenuItem>
-            <MenuItem value="Średnie">Średnie</MenuItem>
-            <MenuItem value="Wysokie">Wysokie</MenuItem>
-          </Select>
-        </FormControl>
+            <FormControl variant="outlined">
+              <InputLabel>Stopień zagrożenia</InputLabel>
+              <Select
+                value={filterThreatLevel}
+                onChange={(e) => setFilterThreatLevel(e.target.value)}
+                label="Stopień zagrożenia"
+              >
+                <MenuItem value="">Wszystkie</MenuItem>
+                <MenuItem value="Niskie">Niskie</MenuItem>
+                <MenuItem value="Średnie">Średnie</MenuItem>
+                <MenuItem value="Wysokie">Wysokie</MenuItem>
+              </Select>
+            </FormControl>
+          </div>
+
+          <ul className={styles.animalList}>
+            {filteredData.map((animal, index) => (
+              <li key={index}>
+                <Typography variant="h5">{animal.name}</Typography>
+                <p>Liczba populacji: {animal.population}</p>
+                <p>Stopień zagrożenia: {animal.threatLevel}</p>
+              </li>
+            ))}
+          </ul>
+        </div>
       </div>
-
-      <ul className={styles.animalList}>
-        {filteredData.map((animal, index) => (
-          <li key={index}>
-            <Typography variant="h5">{animal.name}</Typography>
-            <p>Liczba populacji: {animal.population}</p>
-            <p>Stopień zagrożenia: {animal.threatLevel}</p>
-          </li>
-        ))}
-      </ul>
     </div>
   );
 }
