46e285b00defa0ea9c5dba3a2cc7a47ebe2704dd
Wiktor-Mazur <zegarekpl.gaming@gmail.com>
2023-10-01 02:58:53 +0200

    statistic1

--- src/app/maps/page.tsx
+++ src/app/maps/page.tsx
@@ -1,47 +1,42 @@
-'use client'
-import {CircleF, GoogleMap, MarkerF, useLoadScript} from "@react-google-maps/api";
-import type { NextPage} from "next";
-import {useEffect, useMemo, useState} from "react";
-import {FaMapLocationDot} from "react-icons/fa6";
+"use client";
+import {
+  CircleF,
+  GoogleMap,
+  MarkerF,
+  useLoadScript,
+} from "@react-google-maps/api";
+import type { NextPage } from "next";
+import { useEffect, useMemo, useState } from "react";
 
 const Home: NextPage = () => {
-  const [reps,setReps] = useState([])
-  const libraries = useMemo(() => ['places'], []);
-  const mapCenter = useMemo(
-      () => ({ lat: 50.0678693, lng: 19.9916377 }),
-      []
-  );
-  const map1 = useMemo(
-      () => ({ lat: 50.0678693, lng: 19.9916377 }),
-      []
-  );
-  const map2 = useMemo(
-      () => ({ lat: 50.0678693, lng: 19.9916377 }),
-      []
-  );
+  const [reps, setReps] = useState([]);
+  const libraries = useMemo(() => ["places"], []);
+  const mapCenter = useMemo(() => ({ lat: 50.0678693, lng: 19.9916377 }), []);
+  const map1 = useMemo(() => ({ lat: 50.0678693, lng: 19.9916377 }), []);
+  const map2 = useMemo(() => ({ lat: 50.0678693, lng: 19.9916377 }), []);
 
-  const getAlertLocation =  async () => {
+  const getAlertLocation = async () => {
     const response = await fetch("http://localhost:8080/api/report");
     const reports = await response.json();
-    console.log(reports)
+    console.log(reports);
     setReps(
-        reports.map((rep: any) =>{
-          return {lat: rep.location.latitude, lng: rep.location.longitude}
-        })
-    )
-  }
+      reports.map((rep: any) => {
+        return { lat: rep.location.latitude, lng: rep.location.longitude };
+      })
+    );
+  };
 
   useEffect(() => {
-    getAlertLocation()
-  },[])
+    getAlertLocation();
+  }, []);
 
   const mapOptions = useMemo<google.maps.MapOptions>(
-      () => ({
-        disableDefaultUI: true,
-        clickableIcons: true,
-        scrollwheel: false,
-      }),
-      []
+    () => ({
+      disableDefaultUI: true,
+      clickableIcons: true,
+      scrollwheel: true,
+    }),
+    []
   );
   const { isLoaded } = useLoadScript({
     googleMapsApiKey: "AIzaSyATXEtRMP-Yn6KCPDpHTm5CTiYZI5qWqGc",
@@ -53,30 +48,30 @@ const Home: NextPage = () => {
   }
 
   return (
-      <div>
-        <GoogleMap
-            options={mapOptions}
-            zoom={12}
-            center={mapCenter}
-            mapTypeId={google.maps.MapTypeId.ROADMAP}
-            mapContainerStyle={{ width: '100vw', height: '90vh' }}
-        >
-          {reps.map((rep) => {
-            console.log(rep)
-            return(
-                <CircleF
-                    center={rep}
-                    radius={1000}
-                    options={{
-                      fillColor: 'red',
-                      strokeColor: 'green',
-                      strokeOpacity: 0.8,
-                    }}
-                />
-            )
-          })}
-        </GoogleMap>
-      </div>
+    <div>
+      <GoogleMap
+        options={mapOptions}
+        zoom={12}
+        center={mapCenter}
+        mapTypeId={google.maps.MapTypeId.ROADMAP}
+        mapContainerStyle={{ width: "100vw", height: "90vh" }}
+      >
+        {reps.map((rep) => {
+          console.log(rep);
+          return (
+            <CircleF
+              center={rep}
+              radius={1000}
+              options={{
+                fillColor: "red",
+                strokeColor: "green",
+                strokeOpacity: 0.8,
+              }}
+            />
+          );
+        })}
+      </GoogleMap>
+    </div>
   );
 };
 
